93743b6bddde6edb9b4068924480958dc22b79f3
Maneeshamuthyalapalli <160912657+Maneeshamuthyalapalli@users.noreply.github.com>
2024-03-12 22:36:51 +0530

    Create mini-max-sum.py

---
+++ mini-max-sum.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+def miniMaxSum(arr):
+    arr.sort()
+    min_sum = sum(arr[:4])
+    max_sum = sum(arr[1:])
+    print(min_sum, max_sum)
+arr = list(map(int, input().split()))
+miniMaxSum(arr)
